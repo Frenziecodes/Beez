8f9e810a5733cfd0bfecd94a6b5bebe82b76386b
Frenziecodes <coderflame3@gmail.com>
2023-08-30 00:51:20 +0300

    header requirements

---
+++ beez.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @link              https://github.com/Frenziecodes/Beez/
+ * @since             1.0.0
+ * @package           Beez
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Beez - Business hours management
+ * Plugin URI:        https://github.com/Frenziecodes/Beez/
+ * Description:       Effortlessly display and manage business hours on your website.
+ * Version:           1.0.0
+ * Author:            Lewis ushindi, Frenziecodes
+ * Author URI:        https://github.com/Frenziecodes
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       beez-management
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
