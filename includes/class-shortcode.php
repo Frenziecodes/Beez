54cb69bbca1e9da3ff63a3cc7cab00814b96d53b
frenziecodes <frenziecodes@gmail.com>
2023-09-01 17:15:08 +0300

    update

--- includes/class-shortcode.php
+++ includes/class-shortcode.php
@@ -45,9 +45,9 @@ function beez_business_hours_shortcode( $atts ) {
 	// Create the HTML output
 	$output = '<div class="beez-business-hours" style="border: 1px solid ' . esc_attr( $bg_color ) . '; width: 300px; font-size: '. esc_html( $font_size) .'">';
 		$output .= '<div class="beez-header" style="padding: 2px; display: flex; flex-direction: column; justify-content: space-around; align-items: center; background-color: ' . esc_attr( $bg_color ) . '; color: ' . esc_attr( $text_color ) . ';">';
-			$output .= '<h2>' . esc_html( $title ) . '</h2>';
-			$output .= '<div class="beez-day-date">';
-				$output .= '<p>' . esc_html( $current_day ) . ', ' . esc_html( $current_date ) . '</p>';
+			$output .= '<h2 style="margin: 0;">' . esc_html( $title ) . '</h2>';
+			$output .= '<div class="beez-day-date" style="margin: 0;">';
+				$output .= '<p style="margin: 0;">' . esc_html( $current_day ) . ', ' . esc_html( $current_date ) . '</p>';
 			$output .= '</div>';
 			if ( $is_open ) {
 				$output .= '<span class="open-label" style="background-color: ' . esc_attr( $bg_color ) . '; color: ' . esc_attr( $text_color ) . ';">' . esc_html( $open_label ) . '</span>';
@@ -56,59 +56,61 @@ function beez_business_hours_shortcode( $atts ) {
 			}  
 		$output .= '</div>';        
 		
-		// Display opening and closing hours for each day.
-		$days = array( 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday' );
-		$output .= '<ul class="beez-opening-hours-list">';
-		foreach ( $days as $day ) {
-			$opening_hours = get_option( "beez_opening_hours_$day", '' );
-			$closing_hours = get_option( "beez_closing_hours_$day", '' );
+		$output .= '<div style="background-color: #fee;">'; 
+			// Display opening and closing hours for each day.
+			$days = array( 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday' );
+			$output .= '<ul class="beez-opening-hours-list " style="margin-bottom: 0;">';
+			foreach ( $days as $day ) {
+				$opening_hours = get_option( "beez_opening_hours_$day", '' );
+				$closing_hours = get_option( "beez_closing_hours_$day", '' );
 
-			// Convert hours to selected time format for display only.
-			if ( $time_format === '12-hour' ) {
-				$opening_hours_display = !empty( $opening_hours ) ? date( 'h:i A', strtotime( $opening_hours ) ) : 'Closed';
-				$closing_hours_display = !empty( $closing_hours ) ? date( 'h:i A', strtotime( $closing_hours ) ) : '';
-			} else {
-				$opening_hours_display = !empty( $opening_hours ) ? $opening_hours : 'Closed';
-				$closing_hours_display = !empty( $closing_hours ) ? $closing_hours : '';
+				// Convert hours to selected time format for display only.
+				if ( $time_format === '12-hour' ) {
+					$opening_hours_display = !empty( $opening_hours ) ? date( 'h:i A', strtotime( $opening_hours ) ) : 'Closed';
+					$closing_hours_display = !empty( $closing_hours ) ? date( 'h:i A', strtotime( $closing_hours ) ) : '';
+				} else {
+					$opening_hours_display = !empty( $opening_hours ) ? $opening_hours : 'Closed';
+					$closing_hours_display = !empty( $closing_hours ) ? $closing_hours : '';
+				}
+
+				$output .= '<li><strong>' . esc_html( ucfirst( $day ) ) . ':</strong> ' . esc_html( $opening_hours_display );
+				if ( ! empty( $closing_hours_display ) ) {
+					$output .= ' - ' . esc_html( $closing_hours_display );
+				}
+				$output .= '</li>';
 			}
+			$output .= '</ul>';
 
-			$output .= '<li><strong>' . esc_html( ucfirst( $day ) ) . ':</strong> ' . esc_html( $opening_hours_display );
-			if ( ! empty( $closing_hours_display ) ) {
-				$output .= ' - ' . esc_html( $closing_hours_display );
-			}
-			$output .= '</li>';
-		}
-		$output .= '</ul>';
-
-		$output .= '<div class="beez-display-message" style="margin-top: 20px; text-align: left;">';
-			if ( $is_open ) {
-				$output .= '<p>' . esc_html( $opening_message ) . '</p>';
-			} else {
-				$output .= '<p>' . esc_html( $closing_message ) . '</p>';
-			}
-		$output .= '</div>';
-
-		// Display timezone message if enabled.
-		if ( $display_timezone_message === 'on' ) {
-			$output .= '<div class="">';
-			$output .= '<p>Our hours are displayed in ' . esc_html( $selected_timezone ) . ' timezone.</p>';
+			$output .= '<div class="beez-display-message" style="margin-top: 20px; text-align: left;" style="margin: 0;">';
+				if ( $is_open ) {
+					$output .= '<p>' . esc_html( $opening_message ) . '</p>';
+				} else {
+					$output .= '<p>' . esc_html( $closing_message ) . '</p>';
+				}
 			$output .= '</div>';
-		}
 
-		// Display local time message if enabled.
-		if ( $display_local_time_message === 'on' ) {        
-			$output .= '<div class="">';
-			if (  $time_format === '12-hour'  ) {
-				$current_local_time = date( 'h:i A l', current_time( 'timestamp', true ) );
-				$output .= '<p>Our local time is ' . esc_html( $current_local_time ) . '.</p>';
-			} else{
-				$current_local_time = date( 'H:i l', current_time( 'timestamp', true ) );
-				$output .= '<p>Our local time is ' . esc_html( $current_local_time ) . '.</p>';
+			// Display timezone message if enabled.
+			if ( $display_timezone_message === 'on' ) {
+				$output .= '<div class="">';
+				$output .= '<p style="margin: 0;">Our hours are displayed in ' . esc_html( $selected_timezone ) . ' timezone.</p>';
+				$output .= '</div>';
 			}
-			
-			$output .= '</div>';
-		}
 
+			// Display local time message if enabled.
+			if ( $display_local_time_message === 'on' ) {        
+				$output .= '<div class="">';
+				if (  $time_format === '12-hour'  ) {
+					$current_local_time = date( 'h:i A l', current_time( 'timestamp', true ) );
+					$output .= '<p style="margin: 0;">Our local time is ' . esc_html( $current_local_time ) . '.</p>';
+				} else{
+					$current_local_time = date( 'H:i l', current_time( 'timestamp', true ) );
+					$output .= '<p style="margin: 0;">Our local time is ' . esc_html( $current_local_time ) . '.</p>';
+				}
+				
+				$output .= '</div>';
+			}
+
+		$output .= '</div>'; 
 	$output .= '</div>';
 
 	return $output;
