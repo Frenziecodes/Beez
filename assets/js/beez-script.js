31b4845b373320478adbda581c25e0fc066eec3b
frenziecodes <frenziecodes@gmail.com>
2023-09-01 18:15:23 +0300

    fix Tab Behavior to Initially Display 'Appearance' Tab Only

--- assets/js/beez-script.js
+++ assets/js/beez-script.js
@@ -4,15 +4,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
     tabs.forEach((tab, index) => {
         tab.addEventListener('click', () => {
-            tabs.forEach(t => t.classList.remove('active'));
-            tab.classList.add('active');
+            tabs.forEach(t => t.classList.remove('beez-active-tab'));
+            tab.classList.add('beez-active-tab');
 
             tabContents.forEach(content => content.style.display = 'none');
             tabContents[index].style.display = 'block';
         });
     });
 
-    // Set the first tab as active by default
-    tabs[0].classList.add('active');
-    tabContents[0].style.display = 'block';
+    tabContents.forEach((content, index) => {
+        if (index !== 0) {
+            content.style.display = 'none';
+        }
+    });
+    
+    tabs[0].classList.add('beez-active-tab');
 });
