4aaa53cb8d51332ceeccd84c8efe50105a6df66c
frenziecodes <frenziecodes@gmail.com>
2023-09-06 21:07:52 +0300

    abort direct file call

--- admin/class-beez-settings.php
+++ admin/class-beez-settings.php
@@ -1,5 +1,10 @@
 <?php
 
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+    die;
+}
+
 function beez_get_timezones() {
     $timezones = DateTimeZone::listIdentifiers( DateTimeZone::ALL );
     $timezones_list = array();
